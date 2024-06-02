50ff5b56be8d0c8102872661a6bb8ba2141d2b20
Jean-Baptiste <jbacbanana@gmail.com>
2024-06-02 17:56:41 +0800

    typo

--- watch_agent.py
+++ watch_agent.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
 
     model_type = "PPO"
     models_dir = "models/" + model_type
-    model_watch_name = "PPO_A03_time_optimization_small_cnn_new_reward"
-    model_step = "2075k"
+    model_watch_name = "PPO_A03_time_optimization_small_cnn_from_scratch"
+    model_step = "86k"
 
     model_path = f"{models_dir}/{model_watch_name}/{model_step}"
     model_to_watch = PPO.load(model_path, env=env)
